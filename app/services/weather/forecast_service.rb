904276f1ac1709836f07f5b8790c895e427fa7b5
Sebastian Jimenez <sebasjimenezv@gmail.com>
2025-05-16 13:14:49 -0700

    Forecast service caching by zipcode

--- app/services/weather/forecast_service.rb
+++ app/services/weather/forecast_service.rb
@@ -6,7 +6,12 @@ module Weather
       # @return [Hash] with location, current, forecast and cached_response indicator
       def forecast(address:, builder: API::ClientBuilder)
         client = builder.build_client(name: :weather_api)
-        response = client.forecast(address.postal_code)
+
+        cached_response = true
+        response = Rails.cache.fetch(address.postal_code) do
+          cached_response = false
+          client.forecast(address.postal_code)
+        end
 
         {
           location: response.data.location,
