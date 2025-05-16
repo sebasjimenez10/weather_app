a9a6386ed8c99dfd5269d78715bbcf8bd78e6b2a
Sebastian Jimenez <sebasjimenezv@gmail.com>
2025-05-16 13:14:32 -0700

    Forecast service implementation

--- app/services/weather/forecast_service.rb
+++ app/services/weather/forecast_service.rb
@@ -1,11 +1,19 @@
 module Weather
   class ForecastService
     class << self
+      # ForecastService::forecast
+      # @param [StreetAddress::US] :address The parsed address
+      # @return [Hash] with location, current, forecast and cached_response indicator
       def forecast(address:, builder: API::ClientBuilder)
-      end
+        client = builder.build_client(name: :weather_api)
+        response = client.forecast(address.postal_code)
 
-      def client(builder)
-        @client ||= builder.build_client(name: :weather_api)
+        {
+          location: response.data.location,
+          current: response.data.current,
+          forecast: response.data.forecast,
+          cached_response: cached_response
+        }
       end
     end
   end
