8435f9be84d1c58c0369d980cb50291d001db239
Sebastian Jimenez <sebasjimenezv@gmail.com>
2025-05-16 14:43:47 -0700

    Initial error messages

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -5,7 +5,19 @@ class Forecast
 
   validates_presence_of :address
 
+  validate :address_format
+
   def to_street_address
     StreetAddress::US.parse(address)
+  end
+
+  def address_format
+    if address.present?
+      parsed_address = StreetAddress::US.parse(address)
+
+      if parsed_address.blank?
+        errors.add(:address, "is not a valid address")
+      end
+    end
   end
 end
