9e78c2d04708bdf3b20003878d418f86422bc6b8
Sebastian Jimenez <sebasjimenezv@gmail.com>
2025-05-16 17:16:52 -0700

    Placeholder spec fix

--- spec/services/weather/api/weather_api/client_spec.rb
+++ spec/services/weather/api/weather_api/client_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
-RSpec.describe Weather::Api::WeatherApi::Client do
+RSpec.describe Weather::API::WeatherAPI::Client do
   pending "add some examples to (or delete) #{__FILE__}"
 end
