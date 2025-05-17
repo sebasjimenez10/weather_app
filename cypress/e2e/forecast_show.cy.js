b25c5cfaf065f352e81b41759a0a5f3c2e346cf0
Sebastian Jimenez <sebasjimenezv@gmail.com>
2025-05-17 12:09:36 -0700

    Updated cypress tests

--- cypress/e2e/forecast_show.cy.js
+++ cypress/e2e/forecast_show.cy.js
@@ -1,7 +1,7 @@
 describe("Forecasts index page", () => {
   it("loads successfully", () => {
     cy.visit("/forecasts/8130 Branch Rd%2C Los Angeles%2C CA 90019");
-    cy.contains("Forecast Details");
-    cy.contains("City: Los Angeles");
+    cy.contains("Forecast");
+    cy.contains("Los Angeles");
   });
 });
